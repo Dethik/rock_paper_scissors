3fdec40cc7a502472f9e3c2673d37b6d8e57c2ad
Andy Lawson <alawson89@gmail.com>
2021-02-24 11:03:30 -0800

    add passing 1st spec
    Co-authored-by: Austin Landis <austinlandiscode@gmail.com>
    Co-authored-by: Zack Rutledge <Thorgrim88@gmail.com>

---
+++ lib/rock_paper_scissors.rb
@@ -0,0 +1,35 @@
+class RPS
+  def wins?(arg1, arg2)
+    if arg1 == 'rock' && arg2 == 'scissors'
+      true
+    # elsif arg1 == 'scissors' && arg2 == 'paper'
+    #   true
+    # elsif arg1 == 'paper' && arg2 == 'rock'
+    #   true
+    # elsif arg1 == arg2
+    #   "tie"
+    # else
+    #   false
+    end
+  end
+end
+
+# values = {
+#   'Rock' => {'rock' => 'ties', 'paper' => 'loses', 'scissors' => 'wins'}
+# }
+# rock wins? scissors : paper
+
+
+# def game(user, computer)
+#   def wins
+#     true
+#   end
+#   options = ["rock", "paper", "scissors"]
+
+#   user = gets.chomp.downcase
+#   computer = options.sample
+
+#   if (user == "rock" && computer == "scissors")
+#     user.wins
+#   end
+# end
--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -1,3 +1,10 @@
 require('rspec')
 require('pry')
 require('rock_paper_scissors')
+
+describe('RPS#game') do
+it("returns true if rock is the object and scissors is the argument") do
+  game = RPS.new()
+  expect(game.wins?("rock", "scissors")).to(eq(true))
+  end
+end
