7d55fd38cde4daef3ed4a884c5d1cdf003651fe7
Andy Lawson <alawson89@gmail.com>
2021-02-24 11:07:52 -0800

    add passing spec 2
    Co-authored-by: Austin Landis <austinlandiscode@gmail.com>
    Co-authored-by: Zack Rutledge <Thorgrim88@gmail.com>

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -2,8 +2,8 @@ class RPS
   def wins?(arg1, arg2)
     if arg1 == 'rock' && arg2 == 'scissors'
       true
-    # elsif arg1 == 'scissors' && arg2 == 'paper'
-    #   true
+    elsif arg1 == 'scissors' && arg2 == 'paper'
+      true
     # elsif arg1 == 'paper' && arg2 == 'rock'
     #   true
     # elsif arg1 == arg2
