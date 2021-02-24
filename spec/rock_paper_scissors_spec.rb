b7506e56a619a2307d2b7e777085640d97f462ff
Andy Lawson <alawson89@gmail.com>
2021-02-24 11:06:40 -0800

    add good fail for spec 2
    Co-authored-by: Austin Landis <austinlandiscode@gmail.com>
    Co-authored-by: Zack Rutledge <Thorgrim88@gmail.com>

--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -3,8 +3,12 @@ require('pry')
 require('rock_paper_scissors')
 
 describe('RPS#game') do
-it("returns true if rock is the object and scissors is the argument") do
-  game = RPS.new()
-  expect(game.wins?("rock", "scissors")).to(eq(true))
+  it("returns true if rock is the object and scissors is the argument") do
+    game = RPS.new()
+    expect(game.wins?("rock", "scissors")).to(eq(true))
+  end
+  it("returns true if scissors is the object and paper is the argument") do
+    game = RPS.new()
+    expect(game.wins?("scissors", "paper")).to(eq(true))
   end
 end
